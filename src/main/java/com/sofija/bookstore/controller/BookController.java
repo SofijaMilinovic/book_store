2e4ac7a0d8df28e61550e81f4832c799cd407969
Sofija Milinovic <milinovissofija@gmail.com>
2022-02-06 23:29:28 +0100

    Added CrossOrigin to BookController

--- src/main/java/com/sofija/bookstore/controller/BookController.java
+++ src/main/java/com/sofija/bookstore/controller/BookController.java
@@ -2,6 +2,7 @@ package com.sofija.bookstore.controller;
 
 import com.sofija.bookstore.model.Book;
 import com.sofija.bookstore.service.BookService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Resource
